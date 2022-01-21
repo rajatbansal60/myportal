88aed2ca0f89d341bb95dae18c4a8143d83800a4
rajatbansal60 <rk891811@gmail.com>
2022-01-21 18:41:35 +0530

    dd

--- src/Components/Pages/blog.js
+++ src/Components/Pages/blog.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import bg_img from "../../images/slider_img1.jpg";
+import BlogImg from "../../images/slider_img4.jpg";
 
 export const Blog = () => {
   const BlogList = styled.div`
@@ -34,9 +36,88 @@ export const Blog = () => {
         <section className="blogList py-4">
           <Container className="py-md-4">
             <Row>
-              <Col md={12} className="my-3">
-                <div className="blogbox">
-                  <h2 className="mb-0 text-white fw-bolder fs-1">Blog Name</h2>
+              <Col md={3} className="my-3">
+                <div className="blogbox shadow rounded-3 overflow-hidden">
+                  <Link to="" className="blogImg">
+                    <Image
+                      src={BlogImg}
+                      alt="Blog Image"
+                      className="img-fluid d-block"
+                    />
+                  </Link>
+                  <div className="blog_content p-3">
+                    <h4>Blog Name</h4>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry.
+                    </p>
+                    <Link to="" className="btn btn-primary cusbtn px-3 btn-sm">
+                      Read More
+                    </Link>
+                  </div>
+                </div>
+              </Col>
+              <Col md={3} className="my-3">
+                <div className="blogbox shadow rounded-3 overflow-hidden">
+                  <Link to="" className="blogImg">
+                    <img
+                      src={BlogImg}
+                      alt="Blog Image"
+                      className="img-fluid d-block"
+                    />
+                  </Link>
+                  <div className="blog_content p-3">
+                    <h4>Blog Name</h4>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry.
+                    </p>
+                    <Link to="" className="btn btn-primary cusbtn px-3 btn-sm">
+                      Read More
+                    </Link>
+                  </div>
+                </div>
+              </Col>
+              <Col md={3} className="my-3">
+                <div className="blogbox shadow rounded-3 overflow-hidden">
+                  <Link to="" className="blogImg">
+                    <img
+                      src={BlogImg}
+                      alt="Blog Image"
+                      className="img-fluid d-block"
+                    />
+                  </Link>
+                  <div className="blog_content p-3">
+                    <h4>Blog Name</h4>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry.
+                    </p>
+                    <Link to="" className="btn btn-primary cusbtn px-3 btn-sm">
+                      Read More
+                    </Link>
+                  </div>
+                </div>
+              </Col>
+              <Col md={3} className="my-3">
+                <div className="blogbox shadow rounded-3 overflow-hidden">
+                  <Link to="" className="blogImg">
+                    <img
+                      src={BlogImg}
+                      alt="Blog Image"
+                      className="img-fluid d-block"
+                    />
+                  </Link>
+                  <div className="blog_content p-3">
+                    <h4>Blog Name</h4>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry.
+                    </p>
+                    <Link to="" className="btn btn-primary cusbtn px-3 btn-sm">
+                      Read More
+                    </Link>
+                  </div>
                 </div>
               </Col>
             </Row>
